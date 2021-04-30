fd8606e8a0e65d75c473a06367a0be0bafc4070f
DamienAmbrose <73695967+DamienAmbrose@users.noreply.github.com>
2021-04-30 13:51:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,13 +4,25 @@ var all_cookies = document.cookie
   .reduce((accumulator, [key, value]) => ({ ...accumulator, [key.trim()]: decodeURIComponent(value) }), {});
 
 
+  $(function(){
+    setInterval(oneSecondFunction, 1000);
+    });
+    
+    function oneSecondFunction() {
+      if (all_cookies.cookie != "cookie = declined") {
+        document.getElementById("cookie-container").style.display = block;
+      } else if (all_cookies.cookie != "cookie = allowed") {
+        document.getElementById("cookie-container").style.display = none;
+      }
+    }
+
+
 function setCookie() {
   document.cookie = "cookie = allowed"
+  document.getElementById("cookie-container").style.display = none;
 }
 function removeCookie() {
   document.cookie = "cookie = declined"
-}
-
-if (all_cookies.cookie == "cookie = allowed") {
   document.getElementById("cookie-container").style.display = none;
 }
+
