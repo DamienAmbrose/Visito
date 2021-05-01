235413ece766a76cd6f51b0e83baaece64e03ce1
DamienAmbrose <73695967+DamienAmbrose@users.noreply.github.com>
2021-05-01 19:11:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
+//Allow .noatations from objects
 var all_cookies = document.cookie
   .split(';')
   .map(cookie => cookie.split('='))
   .reduce((accumulator, [key, value]) => ({ ...accumulator, [key.trim()]: decodeURIComponent(value) }), {});
 
 
-      if (all_cookies.cookie == "cookie = declined") {
-        document.getElementById("cookie-container").style.display = "block";
-      } else if (all_cookies.cookie != "cookie = allowed") {
+//Do not show cookie pop up if cookie is allowed
+     if (all_cookies.cookie == "allowed") {
         document.getElementById("cookie-container").style.display = "none";
       }
 
 
-
+//Add and remove cookies
 function setCookie() {
-  document.cookie = "cookie = allowed";
+  document.cookie = "cookie = allowed expires=Fri, 1 Jan 2077 12:00:00 UTC; path=/";
   document.getElementById("cookie-container").style.display = "none";
 }
 function removeCookie() {
-  document.cookie = "cookie = declined";
+  document.cookie = "cookie = declined expires=Fri, 1 Jan 2077 12:00:00 UTC; path=/";
   document.getElementById("cookie-container").style.display = "none";
 }
 
