1158104cb9c68c721779414b87bd5fee420d5531
DamienAmbrose <73695967+DamienAmbrose@users.noreply.github.com>
2021-05-04 20:04:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -14,10 +14,10 @@ var all_cookies = document.cookie
 //Add and remove cookies
 function setCookie() {
   document.cookie = "cookie=allowed; expires=Fri, 1 Jan 2077 12:00:00 UTC; path=/";
-  document.getElementById("cookie-container").style.display = "none";
+  document.getElementById("cookiemonster").style.display = "none";
 }
 function removeCookie() {
   document.cookie = "cookie=declined expires=Fri, 1 Jan 2077 12:00:00 UTC; path=/";
-  document.getElementById("cookie-container").style.display = "none";
+  document.getElementById("cookiemonster").style.display = "none";
 }
 
